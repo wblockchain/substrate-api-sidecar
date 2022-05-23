92f5381adc2e40544d6a221e47f8a6ff7e96c1e5
Jorrin Bruns <curlybracketeffect@gmail.com>
2022-05-23 16:16:24 -0400

    update interface

--- src/types/responses/NicksNickname.ts
+++ src/types/responses/NicksNickname.ts
@@ -1,6 +1,7 @@
 // import { u128 } from '@polkadot/types/primitive';
-// import { IAt } from '.';
 import { Balance } from '@polkadot/types/interfaces';
+
+import { IAt } from '.';
 
 // export interface INickname {
 // 	/**
@@ -14,14 +15,15 @@ import { Balance } from '@polkadot/types/interfaces';
 // }
 
 export interface INickname {
+	at: IAt;
 	/**
 	 * The nickname registered for an address
 	 */
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	nickname: any;
+	nickname: string | null;
 	/**
 	 * The units in which substrate records balances.
 	 */
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	deposit: Balance;
+	deposit: Balance | null;
 }
